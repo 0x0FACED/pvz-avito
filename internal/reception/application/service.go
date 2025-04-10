6c4d3b365160a838a0366da27e4616148e57ae04
0x0FACED <https.genius@gmail.com>
2025-04-10 16:04:22 +0300

    chore: added impl of reception service Create() method of ReceptionService iface, added impl of Create() handler of reception

--- internal/reception/application/service.go
+++ internal/reception/application/service.go
@@ -1,8 +1,13 @@
 package application
 
 import (
+	"context"
+	"errors"
+	"time"
+
 	"github.com/0x0FACED/pvz-avito/internal/pkg/logger"
 	"github.com/0x0FACED/pvz-avito/internal/reception/domain"
+	"github.com/google/uuid"
 )
 
 type ReceptionService struct {
@@ -16,4 +21,34 @@ func NewReceptionService(repo domain.ReceptionRepository, l *logger.ZerologLogge
 		repo: repo,
 		log:  l,
 	}
+}
+
+func (s *ReceptionService) Create(ctx context.Context, params CreateParams) (*domain.Reception, error) {
+	last, err := s.repo.FindLastOpenByPVZ(ctx, params.PVZID)
+	if err != nil {
+		// TODO: handle err
+		return nil, err
+	}
+
+	// TODO: refactor
+	if last != nil {
+		// TODO: handle err
+		return nil, errors.New("there is in progress reception, cant create new one")
+	}
+
+	// all is good, can create new reception
+	reception := domain.Reception{
+		ID:       uuid.NewString(),
+		DateTime: time.Now(),
+		PVZID:    params.PVZID,
+		Status:   domain.InProgress,
+	}
+
+	created, err := s.repo.Create(ctx, &reception)
+	if err != nil {
+		// TODO: handle err
+		return nil, err
+	}
+
+	return created, nil
 }
--- internal/reception/delivery/http/handler.go
+++ internal/reception/delivery/http/handler.go
@@ -1,8 +1,18 @@
 package http
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	auth_domain "github.com/0x0FACED/pvz-avito/internal/auth/domain"
+	"github.com/0x0FACED/pvz-avito/internal/pkg/httpcommon"
+	"github.com/0x0FACED/pvz-avito/internal/reception/application"
+	reception_domain "github.com/0x0FACED/pvz-avito/internal/reception/domain"
+)
 
 type ReceptionService interface {
+	Create(ctx context.Context, params application.CreateParams) (*reception_domain.Reception, error)
 }
 
 type Handler struct {
@@ -20,6 +30,32 @@ func (h Handler) RegisterRoutes(mux *http.ServeMux) {
 }
 
 func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte(`{"message":"not impl"}`))
+	type createRequest struct {
+		PVZID string `json:"pvzId"`
+	}
+
+	var req createRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	claims, ok := r.Context().Value("user").(*httpcommon.Claims)
+	if !ok {
+		http.Error(w, "User not found in context", http.StatusBadRequest)
+		return
+	}
+
+	params := application.CreateParams{
+		PVZID:    req.PVZID,
+		UserRole: auth_domain.Role(claims.Role),
+	}
+
+	reception, err := h.svc.Create(r.Context(), params)
+	if err != nil {
+		httpcommon.JSONError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	httpcommon.JSONResponse(w, http.StatusCreated, reception)
 }
