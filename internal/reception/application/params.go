7117d2f33bdcc00b0555901bbbb99d8e8d296935
0x0FACED <https.genius@gmail.com>
2025-04-10 16:02:27 +0300

    chore: added CreateParams struct for reception

--- internal/reception/application/params.go
+++ internal/reception/application/params.go
@@ -1,1 +1,26 @@
 package application
+
+import (
+	"errors"
+
+	auth_domain "github.com/0x0FACED/pvz-avito/internal/auth/domain"
+	"github.com/google/uuid"
+)
+
+type CreateParams struct {
+	PVZID    string
+	UserRole auth_domain.Role
+}
+
+func (p CreateParams) Validate() error {
+	if err := uuid.Validate(p.PVZID); err != nil {
+		// TODO: handle err
+		return err
+	}
+
+	if p.UserRole != auth_domain.RoleEmployee {
+		return errors.New("only employee can create reception")
+	}
+
+	return nil
+}
