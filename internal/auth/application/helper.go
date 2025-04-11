6db41507c8b1e304cfa87e5ca086f277a513b349
0x0FACED <https.genius@gmail.com>
2025-04-11 17:03:03 +0300

    chore: added funcs to hash password and compare hash and pass

---
+++ internal/auth/application/helper.go
@@ -0,0 +1,26 @@
+package application
+
+import (
+	"fmt"
+
+	auth_domain "github.com/0x0FACED/pvz-avito/internal/auth/domain"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func hashPasswordString(password string, cost int) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return "", fmt.Errorf("%w: %w", auth_domain.ErrHashPassword, err)
+	}
+
+	return string(hash), nil
+}
+
+func compareHashAndPassword(hash string, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return fmt.Errorf("%w: %w", auth_domain.ErrInvalidPassword, err)
+	}
+
+	return nil
+}
