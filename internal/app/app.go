1a921126e7431fe8e49a2ec3a125d9b20b297713
0x0FACED <https.genius@gmail.com>
2025-04-12 15:09:55 +0300

    refactor: added logger to app struct, added metrics server

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,34 +2,47 @@ package app
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"time"
 
 	"github.com/0x0FACED/pvz-avito/internal/pkg/config"
+	"github.com/0x0FACED/pvz-avito/internal/pkg/logger"
 )
 
 type App struct {
-	server *http.Server
-	config *config.AppConfig
+	server        *http.Server
+	metricsServer *http.Server
+	log           *logger.ZerologLogger
+	config        *config.AppConfig
 }
 
-func New(srv *http.Server, cfg *config.AppConfig) *App {
+func New(srv *http.Server, merticsSrv *http.Server, l *logger.ZerologLogger, cfg *config.AppConfig) *App {
 	return &App{
-		server: srv,
-		config: cfg,
+		server:        srv,
+		metricsServer: merticsSrv,
+		log:           l,
+		config:        cfg,
 	}
 }
 
-func (s *App) Start(ctx context.Context) error {
-	errChan := make(chan error, 1)
+func (a *App) Start(ctx context.Context) error {
+	errChan := make(chan error, 2)
 
 	go func() {
-		if err := s.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		a.log.Info().Str("address", a.server.Addr).Msg("Starting application server")
+		if err := a.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			errChan <- err
 		}
-		close(errChan)
 	}()
+
+	if a.config.Metrics.Enabled && a.metricsServer != nil {
+		go func() {
+			a.log.Info().Str("address", a.metricsServer.Addr).Msg("Starting metrics server")
+			if err := a.metricsServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				errChan <- err
+			}
+		}()
+	}
 
 	select {
 	case <-ctx.Done():
@@ -39,13 +52,29 @@ func (s *App) Start(ctx context.Context) error {
 	}
 }
 
-func (s *App) Shutdown() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+func (a *App) Shutdown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err := s.server.Shutdown(ctx); err != nil {
-		return err
+	a.log.Info().Msg("Shutting down servers...")
+
+	var retErr error
+
+	if err := a.server.Shutdown(ctx); err != nil {
+		a.log.Error().Err(err).Msg("Failed to shutdown application server")
+		retErr = err
+	} else {
+		a.log.Info().Msg("Application server stopped")
 	}
 
-	return nil
+	if a.config.Metrics.Enabled && a.metricsServer != nil {
+		if err := a.metricsServer.Shutdown(ctx); err != nil {
+			a.log.Error().Err(err).Msg("Failed to shutdown metrics server")
+			retErr = err
+		} else {
+			a.log.Info().Msg("Metrics server stopped")
+		}
+	}
+
+	return retErr
 }
